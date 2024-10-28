09ac732a6aca09ac810b42e33bab08973c4459ca
Mar-Sia <98830671+Mar-Sia@users.noreply.github.com>
2024-10-28 13:24:33 +0100

    Changed the styling of the primary button

--- src/Components/PrimaryButton.jsx
+++ src/Components/PrimaryButton.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import "../Button.css";
 
 const PrimaryButton = ({ children }) => {
-  const buttonStyle = {
-    backgroundColor: "rgba(228, 115, 71, 1)",
-    color: "white",
-    padding: "10px 20px",
-    borderRadius: "8px",
-    cursor: "pointer",
-  };
-
   return <button className="primary-button">{children}</button>;
 };
 
