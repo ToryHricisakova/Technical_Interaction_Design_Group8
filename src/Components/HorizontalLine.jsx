fe07bc848228d060c300f2683a4dbebe5cc8588e
Luna Quist Poulsen <lunaqpoulsen@gmail.com>
2024-10-31 18:19:38 +0100

    Create horizontal line

---
+++ src/Components/HorizontalLine.jsx
@@ -0,0 +1,10 @@
+const HorizontalLine = ({ width }) => {
+    return ( 
+        <hr style={{
+            width: width, // adjust the width as needed
+            border: "1px solid #E47347",
+        }} />
+     );
+}
+ 
+export default HorizontalLine;
