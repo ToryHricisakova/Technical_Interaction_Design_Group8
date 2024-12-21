b71880f820e7788a8e3f98f2eac715ace2cd3869
ToryHricisakova <viktoria.hricisakova@gmail.com>
2024-12-21 17:54:02 +0100

    LikeIcon commented

--- src/Components/LikeIcon.jsx
+++ src/Components/LikeIcon.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Parse from "parse";
 
@@ -6,17 +6,18 @@ const LikeIcon = ({ objectId, initialLikes }) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(initialLikes);
 
+  // Updates the number of likes for the specified post.
   const updateLikes = async (newLikesCount) => {
     try {
-      // Fetch the post from the database
+      // Fetching the post from the database
       const Post = Parse.Object.extend("POSTS");
       const query = new Parse.Query(Post);
       const post = await query.get(objectId);
 
-      // Update the likes count field
+      // Updating the likes count field
       post.set("numberOfLikes", newLikesCount);
 
-      // Save the post object with the new number of likes
+      // Saving the post object with the new number of likes
       await post.save();
       console.log("Post likes updated successfully");
     } catch (error) {
@@ -24,19 +25,17 @@ const LikeIcon = ({ objectId, initialLikes }) => {
     }
   };
 
+  // Handles the toggle between a liked and unliked post. Only locally.
   const handleLike = () => {
+    // Initializing and setting an unliked post.
     const newLikedState = !liked;
     setLiked(newLikedState);
+    // If newLikedState is true, it increases the likes number, if a person unlikes it, the number decreases.
     const newLikesCount = newLikedState ? likes + 1 : likes - 1;
     setLikes(newLikesCount);
 
     updateLikes(newLikesCount);
   };
-
-  useEffect(() => {
-    // Optionally, fetch the current like state from the backend when the component mounts.
-    // Here you would fetch the current "liked" status of the user, if necessary.
-  }, []);
 
   return (
     <LikeContainer onClick={handleLike}>
